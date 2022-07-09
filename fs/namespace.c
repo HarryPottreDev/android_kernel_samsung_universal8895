fdda8a85f1e4c447d3fce914815ed36036fcc9c8
Anan Jaser <ananjaser@gmail.com>
2022-07-09 19:35:35 +0400

    fs: PAGE_BOOST from N770FXXU8EUH5

--- fs/namespace.c
+++ fs/namespace.c
@@ -2040,6 +2040,10 @@ static inline bool may_mount(void)
  * unixes. Our API is identical to OSF/1 to avoid making a mess of AMD
  */
 
+#ifdef CONFIG_PAGE_BOOST_RECORDING
+#include <linux/io_record.h>
+#endif
+
 SYSCALL_DEFINE2(umount, char __user *, name, int, flags)
 {
 	struct path path;
@@ -2053,6 +2057,9 @@ SYSCALL_DEFINE2(umount, char __user *, name, int, flags)
 	if (!may_mount())
 		return -EPERM;
 
+#ifdef CONFIG_PAGE_BOOST_RECORDING
+	forced_init_record();
+#endif
 	if (!(flags & UMOUNT_NOFOLLOW))
 		lookup_flags |= LOOKUP_FOLLOW;
 
